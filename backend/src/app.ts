5f47979c302d81ff345e53a7d2f0dbc69dece04f
Humildeficador <jvictor.leandro@gmail.com>
2025-07-05 01:39:43 -0300

    feat: some diff in localtunnel

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,19 @@
 import express from 'express'
 import router from './router'
 import cors from 'cors'
+import { TUNNEL } from './utils/tunnel'
 
 export const app = express()
 
-app.use(cors())
+if (TUNNEL) {
+  console.log('Você está usando TUNNEL, não esqueça de abrir a porta no FIREWALL')
+  app.use(cors({
+    origin: 'https://beautyjoao.loca.lt'
+  }))
+} else {
+  app.use(cors())
+}
+
+
 app.use(express.json())
 app.use(router)
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,18 @@
 import { server } from './server'
 import dotenv from 'dotenv'
+import { TUNNEL, IP_LIST } from './utils/tunnel'
 
 dotenv.config()
 
 const PORT = process.env.PORT || 3333
 
-server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`)
-})
+
+if (TUNNEL) {
+  server.listen({ host: '0.0.0.0', port: PORT }, () => {
+    console.log(`Server running on\n${IP_LIST}\nO back-end só aceita requisições no http://localhost:${PORT}`)
+  })
+} else {
+  server.listen({ host: '0.0.0.0', port: PORT }, () => {
+    console.log(`Server running on http://localhost:${PORT}`)
+  })
+}
